30ac19a6fe34c3d925f5691702947c7216a4f80e
vbcode19 <92701457+vbcode19@users.noreply.github.com>
2021-10-22 17:13:29 +0530

    program to check even no or odd no
    In this program we receive a number from the user and check the no is that even or odd

---
+++ Week 1/Question1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main()
+{
+	int num;
+	printf("Enter the number");
+	scanf("%d", &num);
+	if (num % 2 == 0)
+
+	{
+		printf("the no is even no");
+	}
+	else
+	{
+		printf("the no is odd no");
+	}
+}
