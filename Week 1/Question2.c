c4801340eedef1d8c77e06ce419292b51d08a952
vbcode19 <92701457+vbcode19@users.noreply.github.com>
2021-10-22 17:40:34 +0530

    C program to find the maximum between two numbers.
    In this program we can accept two values from the user one is num1 and other is num2 and find the greatest among them

---
+++ Week 1/Question2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int num1,num2;
+    printf("enter the value of numbers");
+    scanf("%d%d",&num1,&num2);
+    if (num1>num2)
+    {
+        printf("num1 is maximum");
+    }
+    else{
+        printf("num2 is maximum");
+    }
+    
+}
