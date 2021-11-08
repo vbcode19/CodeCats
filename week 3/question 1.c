2717cc6dfc488251c92c693f9cd55d1fa8c15e6f
vbcode19 <92701457+vbcode19@users.noreply.github.com>
2021-11-08 17:44:52 +0530

    star pattern

--- week 3/question 1.c
+++ week 3/question 1.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
-main()
-{
-    int i,j;;
-    for(i=0;i<5;i++)
-    {
-        for ( j = 0; j<=i; j++)
-        {
-            printf("*");
-        }
-        printf("\n");
-        
-    }
-}
+#include <stdio.h>  
+ main()  
+{  
+    int k ,i,j,n,m;  
+    printf("Enter the number of rows");  
+    scanf("%d",&n);  
+      
+   for( i=1;i<=n;i++)  
+   {  
+       for( j=1;j<=n;j++)  
+       {  
+           printf(" ");  
+       }  
+       for( k=1;k<=2*i-1;k++)  
+       {  
+         printf("*");  
+       }  
+       n--;  
+     
+      printf("\n");  
+    }  
+   
+}  
